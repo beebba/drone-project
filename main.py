c2d8c32e15a3661137ff6ddcfca247dac244de23
beeb <44314221+beebba@users.noreply.github.com>
2023-10-13 12:57:36 +1100

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+from swarm import Swarm
+
+DEFAULT_DRONE_SCRIPT_FILE_PATH = "shows/default.txt"
+
+
+def main(args: list[str]):
+    path = "shows/" + args[0] if len(args) > 0 else DEFAULT_DRONE_SCRIPT_FILE_PATH
+
+    swarm = Swarm(path)
+
+    swarm.start()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
